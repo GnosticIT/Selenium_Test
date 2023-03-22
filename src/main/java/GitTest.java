dd5f929fd48552e828e4a604b708d8417b6f240f
MAMTech-Limited <128568902+MAMTech-Limited@users.noreply.github.com>
2023-03-22 01:19:50 -0400

    Update GitTest.java (#1)
    * Update GitTest.java
    
    add a comments
    
    * Update GitTest.java
    
    Make the change as per request

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,6 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test File");
-        System.out.println("Git Test File");
+        System.out.println("Git Pull Request Is Working Fine");
+        //checkin from Mam Learning 
     }
 }
